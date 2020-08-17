97f37a2a37a22ebc3737d6751dff228c246de3d5
Jake2.0 <>
2020-08-17 16:25:29 -0500

    TestBuild2

--- src/components/OverviewDashboard.jsx
+++ src/components/OverviewDashboard.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import Header from './Nav/Header';
-import StatBoxesSection from './StatBoxes/StatBoxesSection';
-import GraphSection from './Graphs/GraphSection';
-import GetDataSection from './No Data/GetDataSection';
+import Header from "./Nav/Header";
+import StatBoxesSection from "./StatBoxes/StatBoxesSection";
+import GraphSection from "./Graphs/GraphSection";
+import GetDataSection from "./No Data/GetDataSection";
 
 const useStyles = makeStyles((theme) => ({
-	root: {},
+  root: {},
 }));
 
 const OverviewDashboard = ({ displayData, setDisplayData }) => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<>
-			<Header title='Overview' />
-			<GetDataSection setDisplayData={setDisplayData} />
-			<StatBoxesSection data={displayData} dashboard='overview' />
-			<GraphSection data={displayData} dashboard='overview' />
-		</>
-	);
+  return (
+    <>
+      <Header title="Overview Materials" />
+      <GetDataSection setDisplayData={setDisplayData} />
+      <StatBoxesSection data={displayData} dashboard="overview" />
+      <GraphSection data={displayData} dashboard="overview" />
+    </>
+  );
 };
 
 export default OverviewDashboard;
